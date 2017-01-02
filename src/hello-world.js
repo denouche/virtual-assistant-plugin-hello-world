334c17592a4d0b79875de14cc357af85ad68efce
antoine.leveugle <antoine.leveugle@gmail.com>
2017-01-02 14:46:56 +0100

    first commit

---
+++ src/hello-world.js
@@ -0,0 +1,21 @@
+const AssistantFeature = require('virtual-assistant').AssistantFeature,
+    SlackService = require('virtual-assistant').SlackService;
+
+
+class HelloWorld extends AssistantFeature {
+
+    static getTriggerKeywords() {
+        return [
+            'salut', 'hello', 'coucou', 'hi'
+        ];
+    }
+
+    handle(message, context) {
+        super.handle(message, context);
+        let answer = 'Hello World, how are you ' + SlackService.getDataStore().getUserById(this.context.userId).name;
+        this.send(answer);
+    }
+
+}
+
+module.exports = HelloWorld;
